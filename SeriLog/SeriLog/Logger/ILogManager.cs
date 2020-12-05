15ffcf8f68df8f447c1db5d64c0c4759f97ac25b
Mahsun Emrem <emrem.mahsun@gmail.com>
2020-12-06 02:40:43 +0300

    Finish

---
+++ SeriLog/SeriLog/Logger/ILogManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeriLog.Logger.SeriLog
+{
+    public interface ILogManager
+    {
+        //void Verbose(string message);
+        //void Verbose(string message, T t);
+        //void Fatal(string message);
+        //void Fatal(string message, T t);
+        //void Information(string message);
+        //void Information(string message, T t);
+        //void Warning(string message);
+        //void Warning(string message, T t);
+        //void Debug(string message);
+        //void Debug(string message, T t);
+        //void Error(string message);
+        //void Error(string message, T t);
+    }
+}
---
+++ SeriLog/SeriLog/Logger/LoggingConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeriLog.Logger
+{
+    public abstract class LoggingConfiguration
+    {
+        //protected abstract Logger GetLogger();
+
+        //public Logger InstanceLogger()
+        //{
+        //    return default(Logger);
+        //}
+    }
+
+}
